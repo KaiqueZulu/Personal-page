e58f4f72986a0022a3084cebe72c40c29cbef5d7
Kaique Pires <kaiqueroberto.pires@gmail.com>
2022-05-17 14:03:29 -0300

    Heroku config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.set("view engine", "ejs");
 
@@ -33,6 +34,6 @@ app.get("/gameOver", function (req, res) {
     res.render("pages/gameOver");
 });
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
     console.log("Servidor rodando!");
 });
